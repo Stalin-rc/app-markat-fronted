3e466be0ac4872ce33e35d19eb358b06e62a73a0
Cesar102-web <81375979+Cesar102-web@users.noreply.github.com>
2022-11-13 01:34:38 -0500

    Update dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { Ventas } from './../../models/ventas';
+import { StocksService } from './../../services/stocks/stocks.service';
 import { Stock } from './../../models/stock';
 import { MatTableDataSource } from '@angular/material/table';
 import { VentasService } from './../../services/sales/ventas.service';
@@ -9,33 +11,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
   idStore!: number;
-  ventaTotal:number=0;
-  stockTotal:number=0;
+  ventaTotal: number = 0;
+  stockTotal: number = 0;
   displayedColumns: string[] = ['nameProduct', 'units', 'img'];
   dataSource = new MatTableDataSource<Stock>();
-  
-  constructor(private  ventasService:VentasService, private ActivatedRoute: ActivatedRoute, storesService: StoresService) { }
+  stockNeed!:Stock[];
+  newSales!:Ventas[];
+
+  constructor(
+    private ventasService: VentasService,
+    private ActivatedRoute: ActivatedRoute, private storesService: StoresService, private stocksService:StocksService
+  ) {}
 
   ngOnInit(): void {
+    this.idStore = this.ActivatedRoute.snapshot.params['id'];
 
-  this.idStore = this.ActivatedRoute.snapshot.params['id'];
+    this.ventasService.getTotalSales(this.idStore).subscribe((data: any) => {
+      this.ventaTotal = data;
+    });
 
-  this.ventasService.getTotalSales(this.idStore).subscribe(
-    (data:any)=>{
-      this.ventaTotal=data; 
-  })
+    this.ventasService.getTotalStock(this.idStore).subscribe((data: any) => {
+      this.stockTotal = data;
+    });
 
-  this.ventasService.getTotalStock(this.idStore).subscribe(
-    (data:any)=>{
-      this.stockTotal=data; 
+    this.stocksService.getNeedStock(this.idStore).subscribe((data: Stock[]) => {
+      this.stockNeed= data;
+    });
+
+    this.ventasService.getNewSales(this.idStore).subscribe((data: Ventas[]) => {
+      this.newSales= data;
+    });
+
   }
-)
-
-}
-
 }
